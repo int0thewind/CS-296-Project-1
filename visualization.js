350700ef4162856e5f76805457847a68ea2ce56c
Hanzhi Yin <yinhanzhi@gmail.com>
2019-04-01 22:23:07 -0500

    svg canvas finished

--- visualization.js
+++ visualization.js
@@ -6,13 +6,12 @@ $(function() {
   });
 });
 
-var visualise = function(data) {
-
+var majorVsDepartment = function (data) {
   var majorVsDepartment_margin = { top: 50, right: 50, bottom: 50, left: 100 };
-  var majorVsDepartment_width = 960 - margin.left - margin.right;
-  var MajorVsDepartment_height = 2500 - margin.top - margin.bottom;
+  var majorVsDepartment_width = 960 - majorVsDepartment_margin.left - majorVsDepartment_margin.right;
+  var MajorVsDepartment_height = 2500 - majorVsDepartment_margin.top - majorVsDepartment_margin.bottom;
 
-  var majorVsDepartment_svg = d3.select("#MajorVsDepartment")
+  var majorVsDepartment_svg = d3.select("#majorVsDepartment")
     .append("svg")
     .attr("width", majorVsDepartment_width + majorVsDepartment_margin.left + majorVsDepartment_margin.right)
     .attr("height", MajorVsDepartment_height + majorVsDepartment_margin.top + majorVsDepartment_margin.bottom)
@@ -20,86 +19,25 @@ var visualise = function(data) {
     .style("height", MajorVsDepartment_height + majorVsDepartment_margin.top + majorVsDepartment_margin.bottom)
     .append("g")
     .attr("transform", "translate(" + majorVsDepartment_margin.left + "," + majorVsDepartment_margin.top + ")");
-/*  // Boilerplate:
-      
-  var svg = d3.select("#chart")
+}
+
+var departmentVsCampus = function (data) {
+  var departmentVsCampus_margin = { top: 50, right: 50, bottom: 50, left: 100 };
+  var departmentVsCampus_width = 960 - departmentVsCampus_margin.left - departmentVsCampus_margin.right;
+  var departmentVsCampus_height = 2500 - departmentVsCampus_margin.top - departmentVsCampus_margin.bottom;
+
+  var departmentVsCampus_svg = d3.select("#departmentVsCampus")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .style("width", width + margin.left + margin.right)
-    .style("height", height + margin.top + margin.bottom)
+    .attr("width", departmentVsCampus_width + departmentVsCampus_margin.left + departmentVsCampus_margin.right)
+    .attr("height", departmentVsCampus_height + departmentVsCampus_margin.top + departmentVsCampus_margin.bottom)
+    .style("width", departmentVsCampus_width + departmentVsCampus_margin.left + departmentVsCampus_margin.right)
+    .style("height", departmentVsCampus_height + departmentVsCampus_margin.top + departmentVsCampus_margin.bottom)
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + departmentVsCampus_margin.left + "," + departmentVsCampus_margin.top + ")");
+}
 
-  // Visualization Code:
+var visualise = function(data) {
+  majorVsDepartment(data);
+  departmentVsCampus(data);
 
-  var seasonData = [];
-  var teamData = [];
-
-  for (var i = 0; i < data.length; i++) {
-    if (!seasonData.includes(data[i].Season)) {
-      seasonData.push(data[i].Season)
-    }
-    if (!teamData.includes(data[i].Opponent)) {
-      teamData.push(data[i].Opponent);
-    }
-  }
-
-  var yearScale = d3.scaleLinear()
-                    .domain([d3.min(seasonData),d3.max(seasonData)])
-                    .range([0, width]);
-  var yearAxis = d3.axisTop().scale(yearScale)
-                    .tickValues([1892, 1900, 1960, 1980, 2000, 2010]);
-
-  var teamScale = d3.scalePoint()
-                    .domain(teamData)
-                    .range([0, height]);
-  var teamAxis = d3.axisLeft().scale(teamScale);
-//append axis
-  svg.append("g")
-    .call(yearAxis)
-    .append("g")
-    .call(teamAxis);
-
-  //Append IlliniScore
-  svg.selectAll("IlliniScore")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("r", function(d) {
-      return d.IlliniScore / 5;
-    })      
-    .attr("cx", function (d) {
-      return yearScale( d["Season"]);
-    })
-    .attr("cy", function (d) {
-      return teamScale( d["Opponent"]);
-    })
-    .attr("opacity", 0.5)
-    .attr("stroke", "black")
-    .attr("fill", function(d) {
-      if (d.Result == "W") {
-        return "red";
-      } else return "blue";
-    });
-
-    /*
-    //Append OpponentScore
-    svg.selectAll("OpponentScore")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("r", function(d, i) {
-      return d.OpponentScore / 5;
-    })
-    .attr("cx", function (d, i) {
-      return yearScale(d["Season"])
-    })
-    .attr("cy", function (d, i) {
-      return teamScale( d["Opponent"]);
-    })
-    .attr("opacity", 0.5)
-    .attr("fill", "blue");*/
-
-    //var tip = d3.tip()*/
 };
